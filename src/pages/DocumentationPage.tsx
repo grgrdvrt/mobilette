e523afdbe58979666f524f6b0bef674cc2306772
grgrdvrt <gdivaret@gmail.com>
2025-01-05 12:20:05 +0100

    wip documentation page

--- src/pages/DocumentationPage.tsx
+++ src/pages/DocumentationPage.tsx
@@ -6,7 +6,7 @@ import HomePicto from "../assets/home_FILL0_wght400_GRAD0_opsz24.svg";
 
 export function DocumentationPage(props: { setPage: Setter<string> }) {
   return (
-    <div class="documentation">
+    <div class="documentation" onClick={() => props.setPage("home")}>
       <div class="home-title">
         <h1 class="mainTitle">Mobilette</h1>
         <h2 class="subtitle">.computer</h2>
@@ -16,14 +16,25 @@ export function DocumentationPage(props: { setPage: Setter<string> }) {
       </button>
       <section>
         <h2>Introduction</h2>
-        <p>Mobilette is a simplified programming environment. Its capabilites are limited to basic manipulations on basic datatypes.</lp>
+        <p>
+          Mobilette is a simplified programming environment. Its capabilites are
+          limited to basic manipulations on basic datatypes.
+        </p>
       </section>
       <section>
-        <h2>How to write code</h2>
-        From the Editor, tap on "+" to add an empty line. When an empty line is
-        selected, you can select an instruction. When an instruction is
-        selected, you can access its parameters slots. Tap on a slot to select a
-        register or a value.
+        <h2>Concepts</h2>
+        <p>Instructions</p>
+        <p>Registers</p>
+        <p>Data Types</p>
+      </section>
+      <section>
+        <h2>Programming</h2>
+        <p>
+          From the Editor, tap on "+" to add an empty line. When an empty line
+          is selected, you can select an instruction. When an instruction is
+          selected, you can access its parameters slots. Tap on a slot to select
+          a register or a value.
+        </p>
       </section>
     </div>
   );
